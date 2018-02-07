2ca42336ec931eabdf5778f336014657bb4d45cc
Mahesh Borgude <mahesh.borgude@gmail.com>
2018-02-07 20:22:15 +0530

    new code

--- mahesh/src/com/mahesh/chromebrowser.java
+++ mahesh/src/com/mahesh/chromebrowser.java
@@ -1,5 +1,6 @@
 package com.mahesh;
 
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 
@@ -7,9 +8,15 @@ public class chromebrowser {
 
 
     public static void main(String[] args) {
-        System.setProperty("webdriver.gecko.driver", "C://MAHESH//Selenium//geckodriver.exe");
+        //System.setProperty("webdriver.gecko.driver", "C://MAHESH//Selenium//geckodriver.exe");
+        System.setProperty("webdriver.chrome.driver", "C://MAHESH//Selenium//chromedriver.exe");
         WebDriver driver= new ChromeDriver();
-        driver.
+        driver.get("http://google.com");
+        System.out.println(driver.getCurrentUrl());
+        driver.get("http://www.facebook.com");
+        driver.findElement(By.id("email")).sendKeys("mahesh.borgude@gmail.com");
+        driver.findElement(By.id("pass")).sendKeys("1312129");
+        driver.findElement(By.xpath("//*[@value='Log In']")).click();
     }
 }
 
