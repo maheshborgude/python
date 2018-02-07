870ab2ef81c0199f67e8d650bb51f49b2561a673
Mahesh Borgude <mahesh.borgude@gmail.com>
2018-02-07 18:29:51 +0530

    selenium code

---
+++ mahesh/src/com/mahesh/chromebrowser.java
@@ -0,0 +1,16 @@
+package com.mahesh;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class chromebrowser {
+
+
+    public static void main(String[] args) {
+        System.setProperty("webdriver.gecko.driver", "C://MAHESH//Selenium//geckodriver.exe");
+        WebDriver driver= new ChromeDriver();
+        driver.
+    }
+}
+
+
