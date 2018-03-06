25a885ac0eb759d4a338dc757d0ffd880a384878
Mahesh Borgude <mahesh.borgude@gmail.com>
2018-03-06 11:24:55 +0530

    selenium

--- mahesh/src/com/mahesh/actionsDemo.java
+++ mahesh/src/com/mahesh/actionsDemo.java
@@ -1,7 +1,9 @@
 package com.mahesh;
 
 import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.interactions.Actions;
 
@@ -14,7 +16,12 @@ public class actionsDemo {
         System.setProperty("webdriver.chrome.driver", "C://MAHESH//Selenium//chromedriver.exe");
         WebDriver driver = new ChromeDriver();
         driver.get("https://www.amazon.com");
+
         Actions a=new Actions(driver);
         a.moveToElement(driver.findElement(By.xpath("//a[@id='nav-link-accountList']"))).build().perform();
+        WebElement move = driver.findElement(By.xpath("//a[@id='nav-link-accountList']"));
+        //a.moveToElement(driver.findElement(By.id("twotabsearchtextbox"))).sendKeys("HELLO");
+        a.moveToElement(driver.findElement(By.id("twotabsearchtextbox"))).click().keyDown(Keys.SHIFT).sendKeys("hello").doubleClick().build().perform();
+        a.moveToElement(move).contextClick().build().perform();
     }
 }
---
+++ mahesh/src/com/mahesh/multiwindows.java
@@ -0,0 +1,22 @@
+package com.mahesh;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.interactions.Actions;
+
+import java.util.Set;
+
+public class multiwindows {
+    public static void main(String[] args) {
+        //System.setProperty("webdriver.gecko.driver", "C://MAHESH//Selenium//geckodriver.exe");
+        System.setProperty("webdriver.chrome.driver", "C://MAHESH//Selenium//chromedriver.exe");
+        WebDriver driver = new ChromeDriver();
+        driver.get("https://accounts.google.com");
+        driver.findElement(By.xpath("/html[1]/body[1]/div[1]/div[1]/footer[1]/ul[1]/li[1]/a[1]")).click();
+        System.out.println(driver.getTitle());
+        Set<String>ids=driver.getWindowHandles();
+    }
+}
